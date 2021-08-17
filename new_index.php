993a4d5cce44efa78cc9b7372c9a3ccba2c013a7
mickythegeek <lordkay07@gmail.com>
2021-08-17 17:33:12 +0100

    first commit

---
+++ new_index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<!-- Mirrored from www.designstub.com/demos/onepageresume/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 17 Aug 2021 13:49:04 GMT -->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Michael David's Resume</title>
+    <meta name="description" content="Blvck - Personal vCard & Resume Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/owl.theme.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all">
+
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800" rel="stylesheet">
+
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader-outer">
+        <div class="preloader">
+            <div class="lines">
+                <div class="line line-1"></div>
+                <div class="line line-2"></div>
+                <div class="line line-3"></div>
+            </div>
+
+            <div class="loading-text">- Designstub -</div>
+        </div>
+    </div>
+    <!-- Preloader end -->
+
+    <!-- Main Wrapper -->
+    <main class="wrapper">
+
+        <!-- Header -->
+        <header class="header pull-left">
+
+            <!-- Mobile menu -->
+            <div class="mobile-bar visible-sm visible-xs">
+                <div class="hamburger-menu">
+                    <div class="bar"></div>
+                </div>
+            </div>
+            <!-- Mobile menu end -->
+
+            <!-- <div class="avatar">
+				<img src="assets/images/my-pic.jpg" alt="avatar">
+			</div> -->
+
+            <div class="name">
+                <h1>Michael David</h1>
+                <span>Web Developer</span>
+            </div>
+
+            <!-- Social Icons -->
+            <div class="social-icons">
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+            <!-- Social Icons end -->
+
+            <!-- Navigation bar -->
+            <nav class="main-nav">
+                <ul class="navigation">
+
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#education">Education</a></li>
+
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <!-- Navigation bar end -->
+
+            <!-- Footer Copyright -->
+            <div class="copyright">
+                <span>© Copyright 2021 MiCkY</span>
+            </div>
+            <!-- Footer Copyright end -->
+
+        </header>
+        <!-- Header end -->
+
+        <!-- Main Content -->
+        <div class="main-content pull-right">
+
+
+            <!-- Section About -->
+            <section id="about" class="about">
+
+                <div class="section-header">
+                    <h2>About Me</h2>
+                    <a href="#" class="resume-download" data-toggle="tooltip" data-placement="bottom" title="Download">
+                        <i class="fa fa-download" aria-hidden="true"> </i> Download Resume
+                    </a>
+                </div>
+
+                <!-- Intro -->
+                <div class="intro">
+
+                    <p>Hello, My name is Michael David. I am an energetic and passionate college student working towards a Bachelor's degree in
+                        COMPUTER ENGINEERING at the FEDERAL UNIVERSITY OF TECHNOLOGY, AKURE. I am motivated and enthusiastic by new challenges and tasks and take excellent approach to
+                        achieve success in all projects.</p>
+
+                    <ul class="info">
+                        <li><strong>Birthday:</strong> April 19, 1999</li>
+                        <li><strong>Location:</strong> Akure, Nigeria</li>
+                        <li><strong>Email:</strong> lordkay07@gmail.com</li>
+                        <li><strong>Phone:</strong> +2349031339145</li>
+                    </ul>
+
+
+
+                </div>
+                <!-- Intro end -->
+
+                <!-- Skills -->
+                <div class="skills">
+
+                    <div class="row">
+
+                        <div class="col-md-6 col-sm-6 col-xs-12 item">
+                            <div class="skill-info clearfix">
+                                <h3 class="pull-left">PHP</h3>
+                                <span class="pull-right">65%</span>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:65%">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12 item">
+                            <div class="skill-info clearfix">
+                                <h3 class="pull-left">JavaScript</h3>
+                                <span class="pull-right">55%</span>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:55%">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12 item">
+                            <div class="skill-info clearfix">
+                                <h3 class="pull-left">Photoshop</h3>
+                                <span class="pull-right">50%</span>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12 item">
+                            <div class="skill-info clearfix">
+                                <h3 class="pull-left">Illustrator</h3>
+                                <span class="pull-right">60%</span>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+                <!-- Skills end -->
+            </section>
+            <!-- Section About end -->
+
+
+
+            <!-- Section Resume -->
+            <section id="experience" class="resume">
+
+                <div class="section-header">
+                    <h2>Education</h2>
+
+                </div>
+
+                <div class="row">
+
+
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="top-item resume-item">
+                            <h2>Orchid Inc</h2>
+                            <span>CREATIVE DIRECTOR | JANUARY, 2013 - PRESENT</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime laborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! Nostrum possimus illo architecto praesentium ut aliquam dolorem.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="resume-item">
+                            <h2>VISUAL / UI DESIGNER</h2>
+                            <span>CREATIVE DIRECTOR | MARCH'11 - DECEMBER'12</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime laborum sequi, magni earum quo soluta sint velit numquam.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="resume-item">
+                            <h2>Click Media</h2>
+                            <span>SENIOR UX DESIGNER | JULY'09 - APRIL'11</span>
+                            <p>Odit maxime laborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! Nostrum possimus illo architecto praesentium ut aliquam dolorem.</p>
+                        </div>
+                    </div>
+
+                    <!-- Page 1 End -->
+
+
+
+                </div>
+
+            </section>
+            <!-- Section Resume end -->
+
+
+
+            <!-- Section Resume -->
+            <section id="education" class="resume">
+
+                <div class="section-header">
+                    <h2>Education</h2>
+
+                </div>
+
+                <div class="row">
+
+
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="top-item resume-item">
+                            <h2>Master Degree Of Design</h2>
+                            <span>JANUARY, 2007</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime laborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! Nostrum possimus illo architecto praesentium ut aliquam dolorem.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="resume-item">
+                            <h2>Bachelor Of Arts</h2>
+                            <span>JUNE, 2004</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime laborum sequi, magni earum quo soluta sint velit numquam.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="resume-item">
+                            <h2>Master Degree Of Design</h2>
+                            <span>JANUARY, 2001</span>
+                            <p>Odit maxime laborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! Nostrum possimus illo architecto praesentium ut aliquam dolorem.</p>
+                        </div>
+                    </div>
+
+
+
+                    <!-- Page 1 End -->
+
+
+
+
+                </div>
+
+            </section>
+            <!-- Section Resume end -->
+
+
+
+
+
+            <!-- Section Contact -->
+            <section id="contact" class="contact">
+
+                <div class="section-header">
+                    <h2>Get In Touch</h2>
+                </div>
+
+                <!-- Contact Form -->
+                <form method="post" action="https://www.designstub.com/demos/onepageresume/form/contactform.php">
+
+                    <div class="row">
+                        <!-- Row -->
+
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <!-- Name input -->
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Your Name" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <!-- Email input -->
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Your Email" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <!-- Phone input -->
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="InputPhone" id="InputPhone" placeholder="Phone (optional)">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <!-- Subject input -->
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="InputSubject" name="InputSubject" placeholder="Subject">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <!-- Message textarea -->
+                            <div class="form-group">
+                                <textarea name="InputMessage" id="InputMessage" class="form-control" rows="6" placeholder="Message" required></textarea>
+                            </div>
+                        </div>
+
+                    </div><!-- Row end -->
+
+                    <input type="submit" name="submit" id="submit" value="Send Message" class="btn btn-default pull-left"><!-- Send Button -->
+
+                </form>
+                <!-- Contact Form end -->
+
+            </section>
+            <!-- Section Contact end -->
+
+        </div>
+        <!-- Main Content end -->
+
+    </main>
+    <!-- Main Wrapper end -->
+
+    <!-- SCRIPTS -->
+    <script type="text/javascript" src="assets/js/jquery-1.12.3.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.onepage-scroll.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.backstretch.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.filterizr.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+    <script type="text/javascript" src="assets/js/smoothscroll.min.js"></script>
+
+</body>
+
+<!-- Mirrored from www.designstub.com/demos/onepageresume/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 17 Aug 2021 13:49:26 GMT -->
+
+</html>
+
+
+
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="top-item resume-item">
+        <h2>Livepetal Systems Limited</h2>
+        <span>SOFTWARE DEVELOPER(Intern) | APRIL, 2021 - PRESENT</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime laborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! Nostrum possimus illo architecto praesentium ut aliquam dolorem.</p>
+    </div>
+</div>
